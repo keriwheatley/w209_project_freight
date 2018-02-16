82af20d6ec96b580455b3f2f334d7cc1d98143d3
Daniel Balck <dbalck@us.ibm.com>
2018-02-16 15:25:58 -0500

    added more tests

--- freightwebapp/restapp/tests.py
+++ freightwebapp/restapp/tests.py
@@ -10,8 +10,7 @@ class ShipmentModelTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        print("Hi")
-        pass
+        cls.client = Client()
 
     def setUp(self):
         Shipment.objects.create(foreign_orig=806,
@@ -23,6 +22,16 @@ class ShipmentModelTests(TestCase):
                                 trade_type=2,
                                 value=11,
                                 weight=5
+                                )
+        Shipment.objects.create(foreign_orig=100,
+                                foreign_inbound_mode=2,
+                                domestic_mode=2,
+                                domestic_dest=411,
+                                domestic_state_dest="OR",
+                                commodity=10,
+                                trade_type=1,
+                                value=20,
+                                weight=9
                                 )
 
     def test_shipments_url_exists(self):
@@ -38,7 +47,35 @@ class ShipmentModelTests(TestCase):
         self.assertEqual(len(resp.data), 14)
         self.assertEqual(resp.data['foreign_orig'], 806)
 
+        resp = self.client.get('/shipment/2/')
+        self.assertEqual(len(resp.data), 14)
+        self.assertEqual(resp.data['weight'], 9)
+        self.assertEqual(resp.data['domestic_mode'], '2')
+
     def test_list_all_shipments(self):
         resp = self.client.get('/shipments/')
-        self.assertEqual(len(resp.data), 1)
+        self.assertEqual(len(resp.data), 2)
+
+    def test_create_shipment(self):
+        resp = self.client.post('/shipments/', {'domestic_mode': '1', 'domestic_dest': '111', 'value': '100', 'weight': '200'})
+        self.assertEqual(resp.status_code, 201)
+
+        id = resp.data['id']
+        resp = self.client.get('/shipment/{}/'.format(id))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['weight'], 200)
+
+    def test_delete_shipment(self):
+        resp = self.client.post('/shipments/', {'domestic_mode': '4', 'domestic_dest': '222', 'value': '101', 'weight': '201'})
+        self.assertEqual(resp.status_code, 201)
+        id = resp.data['id']
+        resp = self.client.delete('/shipment/{}/'.format(id))
+        self.assertEqual(resp.status_code, 204)
+
+
+
+
+
+
+
 
