7ba5012452a02184b9dad80d37e9bbd103436d0f
Daniel Balck <dbalck@us.ibm.com>
2018-03-13 16:57:54 -0400

    bar almost working -- still displaying 8 results after click

--- barchart_proto/app.js
+++ barchart_proto/app.js
@@ -1,3 +1,66 @@
+
+var lib = lib || {};
+
+lib.barChartModule = function() {
+  var data = [];
+
+  function data_(_) {
+    var that = this;
+    if (!arguments.length) return data;
+    data = _;
+    return that;
+  }
+
+  function plot_(name, accessor) {
+    y.domain([0, d3.max(data, accessor)]);
+    x.domain(data.map(function(d) {return d.state_origin; }));
+
+    g.select(".title").remove();
+    g.append("text")
+      .attr("class", "title")
+      .text(name);
+
+    g.selectAll("g.axis").remove();
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10));
+
+    var bars = g.selectAll(".bar")
+      .data(data);
+
+
+    bars.transition().duration(300)
+      .attr("x", function(d) { return x(d.state_origin); })
+      .attr("y", function(d) { return y(accessor(d)); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(accessor(d)); });
+
+
+    bars.enter().append("rect")
+        .attr("class", "bar")
+        .attr("id", function(d) {return d.state_origin; })
+        .attr("x", function(d) { return x(d.state_origin); })
+        .attr("y", function(d) { return y(accessor(d)); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(accessor(d)); });
+
+    bars.exit().transition().duration(300).remove();
+
+  }
+
+  return {
+    "data": data_,
+    "plot": plot_
+  };
+};
+
+
 var svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - margin.left - margin.right,
@@ -9,17 +72,6 @@ var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-function update(data, accessor) {
-  g.selectAll("rect")
-    .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.state_origin); })
-      .attr("y", function(d) { return y(accessor(d)); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(accessor(d)); });
-}
-
 d3.csv("sample_data.csv", function(d) {
   d.total_ktons = +d.total_ktons;
   d.total_m_dollar = +d.total_m_dollar;
@@ -27,31 +79,26 @@ d3.csv("sample_data.csv", function(d) {
 }, function(error, data) {
   if (error) throw error;
 
-  var top_ten = data.sort(function(a,b) {return b.total_ktons - a.total_ktons}).slice(0,9);
+  x.domain(data.map(function(d) { return d.state_origin; }));
+
+  var top_ten = data.sort(function(a,b) {return b.total_ktons - a.total_ktons}).slice(0,10);
   //console.log(data[0])
-  x.domain(top_ten.map(function(d) { return d.state_origin; }));
-  y.domain([0, d3.max(data, function(d) { return d.total_ktons; })]);
 
-  g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
 
-  g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10, "%"))
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Frequency");
-
-  update(top_ten, function(d) {
-    return d.total_m_dollar;
+  var myBars = lib.barChartModule();
+  myBars.data(top_ten);
+  myBars.plot("Thousands of Tons",function(d) {
+    return d.total_ktons;
   });
-});
 
-d3.select("#transition").on("click", function() {
+
+  d3.select("#transition").on("click", function() {
+    top_ten = data.sort(function(a,b) {return b.total_m_dollar- a.total_m_dollar}).slice(0,10);
     console.log("clicked!");
+    var myBars = lib.barChartModule();
+    myBars.data(top_ten);
+    myBars.plot("Millions of Dollars", function(d) {
+      return d.total_m_dollar;
+    });
+  });
 });
